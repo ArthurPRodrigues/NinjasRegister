3cad66bf2e516c268e5b3e6ef14a3fed1cef43cf
arthuPRodrigues <arthurprodrigues1@gmail.com>
2025-09-30 19:38:29 -0300

    feat: MissionController http routes

--- src/main/java/dev/java/Missions/MissionController.java
+++ src/main/java/dev/java/Missions/MissionController.java
@@ -1,6 +1,9 @@
 package dev.java.Missions;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class MissionController {
 
-	@GetMapping("/Missions")
+	@GetMapping("/Welcome")
 	public String home() {
-		return "Missions";
+		return "Hello, World!";
+	}
+
+	@GetMapping("/Missions")
+	public String ninjas() {
+		return "These are the Missions!";
+	}
+	@GetMapping("/Missions/id")
+	public String ninjasById() {
+		return "This is the Missions by ID!";
+	}
+
+	@PutMapping("/Missions/update")
+	public String updateNinja() {
+		return "This is the Update Missions!";
+	}
+
+	@PostMapping("/Missions/new")
+	public String newNinja() {
+		return "This is the New Missions!";
+	}
+
+	@DeleteMapping("/Missions/delete")
+	public String deleteNinja() {
+		return "This is the Delete Missions!";
 	}
 }
